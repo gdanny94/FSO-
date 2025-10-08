e6eb1ccb84eb371d9ac860e465b7ca6e461bca4b
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2025-10-07 22:04:35 -0400

    1.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
 import { useState } from "react";
 
-const Stats = ({good, eh, bad}) => {
-  return (
-    <div>
-      <p>Good:{good}</p>
-      <p>Eh:{eh}</p>
-      <p>Bad:{bad}</p>
-      <p>Total:{good + eh + bad}</p>
-      <p>Avg:{(good - bad) / (good + eh + bad)}</p>
-      <p>Positive:{(good * 100) / (good + eh + bad)}%</p>
-    </div>
-  );
-};
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [eh, setEh] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const spin = () => {
+    const findIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(findIndex)
+  };
 
   return (
     <div>
-      <h1>Give Us Feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setEh(eh + 1)}>Eh</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
-      <h3>Results</h3>
-      <Stats good={good} eh={eh} bad={bad} />
+      {anecdotes[selected]}
+      <br />
+      <button onClick={spin}>next anecdote</button>
     </div>
   );
 };
