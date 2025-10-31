545752eb41b854e3fbce2b8e0b29e49a8f2d13c5
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2025-10-31 18:30:29 -0400

    ex.1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,21 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-    </p>
-  );
-};
+import { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [eh, setEh] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content
-        parts={course.parts}
-      />
-      <Total
-        parts={course.parts}
-      />
+      <h1>Give Us Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setEh(eh + 1)}>Eh</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <h3>Results</h3>
+      <p>Good:{good}</p>
+      <p>Eh:{eh}</p>
+      <p>Bad:{bad}</p>
     </div>
   );
 };
